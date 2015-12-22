fdf915e4f56b05ed17a5282499f457efb3698c1c
Christian Høj <christian@skeize.dk>
2015-12-22 15:26:41 +0100

    Update tests to reflect documentation

--- test/promotion_scores_test.js
+++ test/promotion_scores_test.js
@@ -4,55 +4,71 @@ tests["Status code is 200"] = responseCode.code === 200;
 
 if (responseCode.code === 200) {
   var schema = {
-    "$schema": "http://json-schema.org/draft-04/schema#",
-    "id": "/",
-    "type": "object",
-    "properties": {
-      "data": {
-        "id": "data",
-        "type": "array",
-        "items": {
-          "id": "1",
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "id": "/",
+  "type": "object",
+  "properties": {
+    "data": {
+      "id": "data",
+      "type": "object",
+      "properties": {
+        "id": {
+          "id": "id",
+          "type": "string"
+        },
+        "type": {
+          "id": "type",
+          "type": "string"
+        },
+        "attributes": {
+          "id": "attributes",
           "type": "object",
           "properties": {
-            "id": {
-              "id": "id",
-              "type": "string"
-            },
-            "type": {
-              "id": "type",
-              "type": "string"
-            },
-            "attributes": {
-              "id": "attributes",
-              "type": "object",
-              "properties": {
-                "title": {
-                  "id": "title",
-                  "type": "string"
-                }
+            "positions": {
+              "id": "positions",
+              "type": "array",
+              "items": {
+                "id": "8",
+                "type": "object",
+                "properties": {
+                  "position": {
+                    "id": "position",
+                    "type": "integer"
+                  },
+                  "name": {
+                    "id": "name",
+                    "type": "string"
+                  },
+                  "points": {
+                    "id": "points",
+                    "type": "string"
+                  },
+                  "image": {
+                    "id": "image",
+                    "type": "string"
+                  }
+                },
+                "additionalProperties": false
               },
-              "additionalProperties": false
+              "additionalItems": false
             }
           },
-          "additionalProperties": false,
-          "required": [
-            "id",
-            "type",
-            "attributes"
-          ]
-        },
-        "additionalItems": false,
-        "required": [
-          "1"
-        ]
-      }
-    },
-    "additionalProperties": false,
-    "required": [
-      "data"
-    ]
-  };
+          "additionalProperties": false
+        }
+      },
+      "additionalProperties": false,
+      "required": [
+        "id",
+        "type",
+        "attributes"
+      ]
+    }
+  },
+  "additionalProperties": false,
+  "required": [
+    "data"
+  ]
+};
 
   var response = JSON.parse(responseBody);
 
